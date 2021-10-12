a1436fc88e3d844de39f1dd987a77f63fc2d3471
Akbar <akbar@cybertekschool.com>
2021-10-12 15:19:04 -0400

    Cucumber Day 04: Clean up the step def method to make it easy to read and maintain

--- src/test/java/com/cydeo/step_definitions/WebOrderStepDefs.java
+++ src/test/java/com/cydeo/step_definitions/WebOrderStepDefs.java
@@ -27,24 +27,8 @@ public class WebOrderStepDefs {
         System.out.println("expectedOptions = " + expectedOptions);
         WOrderPage orderPage = new WOrderPage();
 
-        WebElement dropdown = orderPage.productDropdown ;
-        Select selectObj = new Select(dropdown);
+        List<String> actualOptions = orderPage.getAllProductOptionFromList();
 
-//        List<String> actualOptions =
-//                selectObj.getOptions().stream()
-//                        .map(eachOption-> eachOption.getText())
-//                        .collect(Collectors.toList());
-
-        //getOptions method from select class is used
-        // to return all dropdown options as List of webelement
-        List<WebElement> allProductOptions = selectObj.getOptions();
-        // this is the list to store actual option so we can compare with expected
-        List<String> actualOptions = new ArrayList<>();
-
-        for (WebElement eachOption : allProductOptions) {
-            System.out.println("eachOption.getText() = " + eachOption.getText());
-            actualOptions.add(eachOption.getText()) ;
-        }
         // assert these two list are equal
         // import static org.junit.Assert.assertEquals;
         assertEquals(expectedOptions, actualOptions) ;

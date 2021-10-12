ad1649771f09e8503f68b4057cff170a7bc56878
Akbar <akbar@cybertekschool.com>
2021-10-12 16:23:42 -0400

    Cucumber Day 04: library_login step definitions

---
+++ src/test/java/com/cydeo/step_definitions/LibraryLoginStepDefs.java
@@ -0,0 +1,20 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LibraryLoginStepDefs {
+    @Given("user is at library login page")
+    public void userIsAtLibraryLoginPage() {
+    }
+
+
+    @Then("user should be at dashboard page")
+    public void userShouldBeAtDashboardPage() {
+    }
+
+    @When("user use username {string} and passcode {string}")
+    public void userUseUsernameAndPasscode(String email, String password) {
+    }
+}

87de4b0753f0307d357b7889aaae3d4ff128a7cd
Akbar <akbar@cybertekschool.com>
2021-10-14 11:51:38 -0400

    Cucumber Day 05 : Fixed the navigation step

--- src/test/java/com/cydeo/step_definitions/WebOrderStepDefs.java
+++ src/test/java/com/cydeo/step_definitions/WebOrderStepDefs.java
@@ -1,5 +1,6 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.WAllProductPage;
 import com.cydeo.pages.WCommonArea;
 import com.cydeo.pages.WOrderPage;
 import io.cucumber.java.en.Then;
@@ -9,6 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
@@ -18,8 +20,23 @@ public class WebOrderStepDefs {
     public void weSelectTabFromSidebar(String tabName) {
         System.out.println("tabName = " + tabName);
         WCommonArea commonArea = new WCommonArea();
-        commonArea.orderTab.click();
+       switch (tabName) {
+           case "View all products" :
+               commonArea.viewAllProductTab.click();
+               break;
+           case "View all orders" :
+                commonArea.viewAllOrderTab.click();
+               break;
+           case "Order":
+                commonArea.orderTab.click();
+               break;
+       }
+
     }
+
+
+
+
 
     @Then("we should see below options in Product dropdown list")
     public void weShouldSeeBelowOptionsInProductDropdownList(List<String> expectedOptions) {
@@ -32,5 +49,17 @@ public class WebOrderStepDefs {
         // assert these two list are equal
         // import static org.junit.Assert.assertEquals;
         assertEquals(expectedOptions, actualOptions) ;
+    }
+
+    @Then("we should see table with below content")
+    public void weShouldSeeTableWithBelowContent(List<Map<String,String>> productMapLst) {
+
+        System.out.println("productMapLst = " + productMapLst);
+
+        WAllProductPage allProductPage = new WAllProductPage();
+
+
+
+
     }
 }

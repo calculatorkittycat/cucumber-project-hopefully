6191c3b08221eaaa308382e65096655c17d43bae
Akbar <akbar@cybertekschool.com>
2021-10-14 16:15:54 -0400

    Cucumber Day 05 : Added rerun plugin and PrettyReport plugin for fancy report

--- src/test/java/com/cydeo/runner/TestRunner.java
+++ src/test/java/com/cydeo/runner/TestRunner.java
@@ -20,9 +20,13 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(  features = "src/test/resources/features" , //alternatively "classpath:features"
                    glue = "com/cydeo/step_definitions" ,
-                   plugin = {"pretty", "html:target/cucumber.html"  } ,
+                    publish = true, // it will give you public link of your local html report
+                   plugin = {"pretty", "html:target/cucumber.html" ,
+                            "rerun:target/rerun.txt" ,  // store the failed scenario into rerun.txt
+                           "me.jvt.cucumber.report.PrettyReports:target"  // fancy report
+                   } ,
                    dryRun = false
-                   , tags = "@go_home"
+//                   , tags = "@bla"
                 )
 public class TestRunner {
 }
